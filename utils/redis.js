6880a141eab53716f9f82e1615f44ff63af21d43
siphoprince <mabirimisesipho@gmail.com>
2024-05-10 04:46:06 -0700

    redis connection error

--- utils/redis.js
+++ utils/redis.js
@@ -1,55 +1,37 @@
 import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor() {
+    this.client = redis.createClient();
 
-        this.client.on('error', (err) => {
-            console.error('Redis Error:', err);
-        });
-    }
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+    this.clientGet = promisify(this.client.get).bind(this.client);
+    this.clientSet = promisify(this.client.set).bind(this.client);
+    this.clientDel = promisify(this.client.del).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, reply) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(reply);
-                }
-            });
-        });
-    }
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        return new Promise((resolve, reject) => {
-            this.client.setex(key, duration, value, (err, reply) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(reply);
-                }
-            });
-        });
-    }
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (err, reply) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(reply);
-                }
-            });
-        });
-    }
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
-
 export default redisClient;
