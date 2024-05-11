19d6833d0ca95944f2840aea2096cdfac665501e
siphoprince <mabirimisesipho@gmail.com>
2024-05-11 04:48:54 -0700

    sending from index to controlls

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,28 @@ import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+router.get('/status', (req, res) => {
+  AppController.getStatus(req, res);
+});
+
+router.get('/stats', (req, res) => {
+  AppController.getStats(req, res);
+});
+
+router.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
+});
+
+router.get('/connect', (req, res) => {
+  AuthController.getConnect(req, res);
+});
+
+router.get('/disconnect', (req, res) => {
+  AuthController.getDisconnect(req, res);
+});
+
+router.get('/users/me', (req, res) => {
+  UsersController.getMe(req, res);
+});
 
 export default router;
