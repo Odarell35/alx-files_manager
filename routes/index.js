de7789642311a1c29441cb106e73b0986660908b
siphoprince <mabirimisesipho@gmail.com>
2024-05-13 05:25:39 -0700

    revers index

--- routes/index.js
+++ routes/index.js
@@ -5,38 +5,28 @@ import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
-const routeController = (app) => {
-  app.use('/', router);
+router.get('/status', (req, res) => {
+  AppController.getStatus(req, res);
+});
 
-  // App Controller
+router.get('/stats', (req, res) => {
+  AppController.getStats(req, res);
+});
 
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+router.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
+});
 
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+router.get('/connect', (req, res) => {
+  AuthController.getConnect(req, res);
+});
 
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
+router.get('/disconnect', (req, res) => {
+  AuthController.getDisconnect(req, res);
+});
 
-  router.get('/connect', (req, res) => {
-    AuthController.getConnect(req, res);
-  });
+router.get('/users/me', (req, res) => {
+  UsersController.getMe(req, res);
+});
 
-  router.get('/disconnect', (req, res) => {
-    AuthController.getDisconnect(req, res);
-  });
-
-  router.get('/users/me', (req, res) => {
-    UsersController.getMe(req, res);
-  });
-
-  // router.post('/files', (req, res) => {
-  //   FilesController.postUpload(req, res);
-  // });
-};
-
-export default routeController;
+export default router;
