cf175e447f662ae08315d7f637b3f526f5328ac0
siphoprince <mabirimisesipho@gmail.com>
2024-05-13 12:12:49 -0700

    using modules instead of export

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,35 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-
+const express = require('express');
 const router = express.Router();
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 const routeController = (app) => {
   app.use('/', router);
 
-router.get('/status', (req, res) => {
-  AppController.getStatus(req, res);
-});
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
 
-router.get('/stats', (req, res) => {
-  AppController.getStats(req, res);
-});
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
 
-router.post('/users', (req, res) => {
-  UsersController.postNew(req, res);
-});
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
 
-router.get('/connect', (req, res) => {
-  AuthController.getConnect(req, res);
-});
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
 
-router.get('/disconnect', (req, res) => {
-  AuthController.getDisconnect(req, res);
-});
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
 
-router.get('/users/me', (req, res) => {
-  UsersController.getMe(req, res);
-});
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
+};
 
-export default router;
+module.exports = routeController;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(routes);
+routes(app);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
