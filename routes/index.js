8d6de5db3c935d9ef0962617c56d0934bd06555d
siphoprince <mabirimisesipho@gmail.com>
2024-05-13 05:17:17 -0700

    fixing index

--- routes/index.js
+++ routes/index.js
@@ -8,28 +8,35 @@ const router = express.Router();
 const routeController = (app) => {
   app.use('/', router);
 
-router.get('/status', (req, res) => {
-  AppController.getStatus(req, res);
-});
+  // App Controller
 
-router.get('/stats', (req, res) => {
-  AppController.getStats(req, res);
-});
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
 
-router.post('/users', (req, res) => {
-  UsersController.postNew(req, res);
-});
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
 
-router.get('/connect', (req, res) => {
-  AuthController.getConnect(req, res);
-});
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
 
-router.get('/disconnect', (req, res) => {
-  AuthController.getDisconnect(req, res);
-});
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
 
-router.get('/users/me', (req, res) => {
-  UsersController.getMe(req, res);
-});
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
 
-export default router;
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
+
+  // router.post('/files', (req, res) => {
+  //   FilesController.postUpload(req, res);
+  // });
+};
+
+export default routeController;
