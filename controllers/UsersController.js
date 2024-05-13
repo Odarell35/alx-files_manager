320072aa1b40c906c74cad28fd5a04dc165eba14
siphoprince <mabirimisesipho@gmail.com>
2024-05-13 05:13:51 -0700

    fixing controolers users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,56 +4,53 @@ import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
 
 class UsersController {
-  static async createUser(request, response) {
+  static async postNew(request, response) {
     const { email, password } = request.body;
     if (!email) {
-      return response.status(400).json({ error: 'Missing email' });
+      response.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      return response.status(400).json({ error: 'Missing password' });
+      response.status(400).json({ error: 'Missing password' });
     }
 
-    const hashedPassword = sha1(password);
+    const hashPwd = sha1(password);
 
     try {
-      const usersCollection = dbClient.db.collection('users');
-      const existingUser = await usersCollection.findOne({ email });
+      const collection = dbClient.db.collection('users');
+      const user1 = await collection.findOne({ email });
 
-      if (existingUser) {
-        return response.status(400).json({ error: 'User already exists' });
+      if (user1) {
+        response.status(400).json({ error: 'Already exist' });
       } else {
-        const newUser = await usersCollection.insertOne({ email, password: hashedPassword });
-        return response.status(201).json({ id: newUser.insertedId, email });
+        collection.insertOne({ email, password: hashPwd });
+        const newUser = await collection.findOne(
+          { email },
+          { projection: { email: 1 } },
+        );
+        response.status(201).json({ id: newUser._id, email: newUser.email });
       }
     } catch (error) {
-      console.error('Error creating user:', error);
-      return response.status(500).json({ error: 'Server error' });
+      console.log(error);
+      response.status(500).json({ error: 'Server error' });
     }
   }
 
-  static async getUser(request, response) {
+  static async getMe(request, response) {
     try {
       const userToken = request.header('X-Token');
-      if (!userToken) {
-        return response.status(401).json({ error: 'Unauthorized' });
-      }
-      
       const authKey = `auth_${userToken}`;
-      const userId = await redisClient.get(authKey);
-      
-      if (!userId) {
-        return response.status(401).json({ error: 'Unauthorized' });
+      // console.log('USER TOKEN GET ME', userToken);
+      const userID = await redisClient.get(authKey);
+      console.log('USER KEY GET ME', userID);
+      if (!userID) {
+        response.status(401).json({ error: 'Unauthorized' });
       }
-
-      const user = await dbClient.getUser({ _id: ObjectId(userId) });
-      if (!user) {
-        return response.status(404).json({ error: 'User not found' });
-      }
-
-      return response.json({ id: user._id, email: user.email });
+      const user = await dbClient.getUser({ _id: ObjectId(userID) });
+      // console.log('USER GET ME', user);
+      response.json({ id: user._id, email: user.email });
     } catch (error) {
-      console.error('Error getting user:', error);
-      return response.status(500).json({ error: 'Server error' });
+      console.log(error);
+      response.status(500).json({ error: 'Server error' });
     }
   }
 }
